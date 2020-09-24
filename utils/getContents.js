5af74529303d602527dac6024d038602bddeb9a3
Yoichi Sato <nfoxnetwork@gmail.com>
2020-09-24 09:56:38 +0900

    fix encoding convert

--- utils/getContents.js
+++ utils/getContents.js
@@ -1,12 +1,19 @@
-const axios = require('axios'),
-      charsetConverter = require('./charsetConverter')
-      ;
+const axios = require('axios');
+const Iconv = require('iconv').Iconv
+const Encoding = require('encoding-japanese')
 
 module.exports = function getContents(url, { headers = {} } = {}) {
     return new Promise((resolve, reject) => {
-        axios.get(url, { headers }).then(res => {
-            const data = charsetConverter(res.data);
-            resolve(data);
+        axios.get(url, {
+            headers,
+            responseType: 'arraybuffer',
+            transformResponse : [(data) => {
+                const iconv = new Iconv(Encoding.detect(data), 'UTF-8//TRANSLIT//IGNORE')
+                const converted = iconv.convert(data).toString()
+                return converted
+            }]
+        }).then(res => {
+            resolve(res.data);
         }).catch(err => {
             reject(err);
         })
